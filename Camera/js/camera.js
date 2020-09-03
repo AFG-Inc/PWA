ee0fc7a7ede8e1155c6701de89846c98d5a7df13
ベロフ　アレクサンドル <abelov@milize.co.jp>
2020-09-03 20:11:48 +0900

    白黒フィルタ

--- Camera/js/camera.js
+++ Camera/js/camera.js
@@ -1,5 +1,16 @@
+var video = document.querySelector('video');
+video.hidden = true;
+var canvas=document.querySelector('canvas');
+var labe =document.getElementById('labe');
+var shaba =document.getElementById('shaba');
+var w,h,leftPos;
+var context=canvas.getContext('2d');
+resiz();
+
 function openCamera() {
-    var video = document.querySelector('video');
+
+
+
     navigator.mediaDevices = navigator.mediaDevices
     || ((navigator.mozGetUserMedia 
     || navigator.webkitGetUserMedia) ? {
@@ -10,7 +21,7 @@ function openCamera() {
             });
         }
     } : null);
-    var constraints = { video: { facingMode: 'environment', width: 720, height: 720 } };
+    var constraints = { video: { facingMode: 'environment', width: 480, height: 640 } };
     navigator.mediaDevices.getUserMedia(constraints)
         .then(function(stream) {
             video.srcObject = stream;
@@ -21,4 +32,50 @@ function openCamera() {
         .catch(function(err) {
             console.log(err);
         });
+
+    snap();
+
+}
+
+function snap() {
+    context.drawImage(video,0,0,w,h);
+
+    var x = Math.round(canvas.width / 4.0);
+    var y = Math.round(x * 2.0);
+
+    var imageData = context.getImageData(x, x, y, y);
+    var data = imageData.data;
+    var sred;
+ 
+    for (var i = 0; i < data.length; i += 4) {
+      sred = Math.round((data[i] + data[i + 1] + data[i + 2]) / 3.0);
+      data[i]     = sred;
+      data[i + 1] = sred;
+      data[i + 2] = sred;
+     }
+    context.putImageData(imageData, x, x);
+
+    setTimeout(snap, 10);
+}
+
+function resiz() {
+    var aspect = window.innerWidth / window.innerHeight;
+    if (aspect < 0.75){
+       w = Math.round(window.innerWidth);
+       h = Math.round(window.innerWidth / 0.75);
+       leftPos = 0;
+    } else {
+       w = Math.round(window.innerHeight * 0.75);
+       h = Math.round(window.innerHeight);
+       leftPos = Math.round((window.innerWidth - w) / 2.0)
+    }
+    labe.innerText = w + " " + h;
+    canvas.width   = w;
+    canvas.height  = h;
+    canvas.style.position = "absolute";
+    canvas.style.left = leftPos + "px";  
+    shaba.width   = w;
+    shaba.height  = h;
+    shaba.style.position = "absolute";
+    shaba.style.left = leftPos + "px";  
 }
