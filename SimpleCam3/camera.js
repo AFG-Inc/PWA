2a2bff91eada186c84062309d99c65aa84118fd3
ベロフ　アレクサンドル <abelov@milize.co.jp>
2020-09-10 15:17:57 +0900

    SimpleCam3-8

--- SimpleCam3/camera.js
+++ SimpleCam3/camera.js
@@ -9,7 +9,7 @@ resiz();
 snap();
 
 function openCamera() {
-	
+	resiz();
     var constraints = { audio: false, video: { facingMode: 'environment' } };
     navigator.mediaDevices.getUserMedia(constraints)
         .then(function(stream) {
