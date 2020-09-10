d5069f7c69ffe248ac927d606ea6c76acb24e93d
ベロフ　アレクサンドル <abelov@milize.co.jp>
2020-09-10 14:52:13 +0900

    SimpleCam3-5

---
+++ SimpleCam3/sw.js
@@ -0,0 +1,22 @@
+var CACHE_NAME = 'pwa-camera-3';
+var urlsToCache = ['/index.html', '/camera.js'];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
