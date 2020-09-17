11b1334d167760abd7803e85b04f4f7cc0cb9b56
ベロフ　アレクサンドル <abelov@milize.co.jp>
2020-09-17 18:20:26 +0900

    Nenkin4

--- Nenkin/sw.js
+++ Nenkin/sw.js
@@ -1,5 +1,5 @@
 var CACHE_NAME = 'nenkin-test-caches';
-var urlsToCache = ['index.html', 'camera.js'];
+var urlsToCache = ['/index.html', '/camera.js'];
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
