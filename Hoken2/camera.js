2766127b583643a213c791a9bb02159b4e9eaece
ベロフ　アレクサンドル <abelov@milize.co.jp>
2020-12-04 07:32:03 +0900

    Update camera.js

--- Hoken2/camera.js
+++ Hoken2/camera.js
@@ -3,7 +3,7 @@
 // コード: Alexander Belov
 "use strict";
 
-let version   = 1.01;
+let version   = 1.02;
 let video     = document.getElementById('video');
 let mainimg   = document.getElementById('mainimg');
 let canvas    = document.getElementById('canvas');
