9cbd45f9c77c630edcef55393e7296f768e13a90
ベロフ　アレクサンドル <abelov@milize.co.jp>
2020-12-03 16:38:56 +0900

    Update camera.js

--- SimpleCam6/camera.js
+++ SimpleCam6/camera.js
@@ -63,7 +63,7 @@ function resiz() {
        leftPos = Math.round((window.innerWidth - w) / 2.0)
     }
 
-	labe.innerText = w + " x " + h + "  ASP:" + videoasp + " Video: " + ww + " x " + hh;
+	labe.innerText = w + " x " + h + "  ASPa:" + videoasp + " Video: " + ww + " x " + hh;
     canvas.width   = w;
     canvas.height  = h;
     canvas.style.left = leftPos + "px";  
